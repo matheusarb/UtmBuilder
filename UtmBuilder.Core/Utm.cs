1da869c8b9344f274879f5d1421c2bac254aeb4a
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-24 18:50:48 -0300

    adding summaries

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -5,7 +5,7 @@ namespace UtmBuilder.Core;
 
 public class Utm
 {
-    public Url Url { get; set; } = new();
-    public Campaign Campaign { get; set; } = new();
+    public Url Url { get; set; }
+    public Campaign Campaign { get; set; }
 
 }
--- UtmBuilder.Core/ValueObjects/Campaign.cs
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -2,10 +2,34 @@ namespace UtmBuilder.Core.ValueObjects;
 
 public class Campaign : ValueObject
 {
-    public string? Source { get; set; }
-    public string? Medium { get; set; }
-    public string? Name { get; set; }
+    public Campaign(
+        string source,
+        string medium,
+        string name,
+        string? id = null,
+        string? term = null,
+        string? content = null)
+    {
+        Source = source;
+        Medium = medium;
+        Name = name;
+        Id = id;
+        Term = term;
+        Content = content;
+    }
+
+    /// <summary>
+    /// The referrer (e.g. google, newsletter)
+    /// </summary>
+    public string Source { get; set; }
+    public string Medium { get; set; }
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The ads campaign id.
+    /// </summary>
     public string? Id { get; set; }
+
     public string? Term { get; set; }
     public string? Content { get; set; }
 }
