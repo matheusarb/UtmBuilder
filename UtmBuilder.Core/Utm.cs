c708c6cc8e8bba384b29f7a4010f5e1e3d014bfe
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-24 22:29:47 -0300

    utm operator

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,6 +1,7 @@
 
 using UtmBuilder.Core.ExtensionMethods;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core;
 
@@ -22,9 +23,35 @@ public class Utm
     /// </summary>
     public Campaign Campaign { get; }
 
-    //Implicit Operator
+    //Implicit Operators
     public static implicit operator string(Utm utm)
         => utm.ToString();
+
+    public static implicit operator Utm(string link)
+    {
+        if (string.IsNullOrEmpty(link))
+            throw new InvalidUrlException();
+
+        var url = new Url(link);
+        var segments = url.Address.Split("?");
+        if (segments.Length < 2)
+            throw new InvalidUrlException("No segments provided");
+
+        var pars = segments[1].Split("&");
+        var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault().Split("=")[1];
+        var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault();
+        var campaign = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault();
+        var id = pars.Where(x => x.StartsWith("utm_id")).FirstOrDefault();
+        var term = pars.Where(x => x.StartsWith("utm_term")).FirstOrDefault();
+        var content = pars.Where(x => x.StartsWith("utm_content")).FirstOrDefault();
+
+        var utm = new Utm(
+            new Url(segments[0]),
+            new Campaign(source, medium, campaign, id, term, content)
+        );
+
+        return utm;
+    }
 
     public override string ToString()
     {
