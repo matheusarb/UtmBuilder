79f8aca8533fdd3abdb3c8bf4b252c19db3cf252
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-25 13:15:55 -0300

    refactoring utmTest

--- UtmBuilder.Core.Tests/UtmTests.cs
+++ UtmBuilder.Core.Tests/UtmTests.cs
@@ -10,14 +10,21 @@ namespace UtmBuilder.Core.Tests;
 [TestClass]
 public class UtmTests
 {
-    private Url link = new Url("https://example.io");
-    private Campaign campaign = new Campaign("source", "medium", "name", "id", "term", "content");
+    private Url url = new Url("https://example.io");
+    private Campaign campaign = new Campaign("src", "med", "nme", "id", "trm", "cnt");
 
     [TestMethod]
     public void Should_return_URL_from_utm()
     {
-        var utm = new Utm(link, campaign);
-        utm.ToString();
-        Assert.IsTrue(true);
+        var utm = new Utm(url, campaign);
+        var result = "https://example.io" +
+            "?utm_source=src" +
+            "&utm_medium=med" +
+            "&utm_campaign=nme" +
+            "&utm_id=id" +
+            "&utm_term=trm" +
+            "&utm_content=cnt";
+
+        Assert.AreEqual(result, utm.ToString());
     }
 }
