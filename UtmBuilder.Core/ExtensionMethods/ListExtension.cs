5e3db11ebb22c59d587f0f0910ec71755162d786
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-24 21:23:17 -0300

    add extensionMethod

--- UtmBuilder.Core/ExtensionMethods/ListExtension.cs
+++ UtmBuilder.Core/ExtensionMethods/ListExtension.cs
@@ -3,8 +3,11 @@ namespace UtmBuilder.Core.ExtensionMethods;
 public static class ListExtension
 {
     public static void AddIfNotNull(
-        this List<string> list)
+        this List<string> list,
+        string key,
+        string? value)
     {
-
+        if(!string.IsNullOrEmpty(value))
+            list.Add($"{key}={value}");
     }
 }
--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -25,6 +25,12 @@ public class Utm
     public override string ToString()
     {
         var segments = new List<string>();
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term);
+        segments.AddIfNotNull("utm_content", Campaign.Content);
 
         return $"{Url.Address}?{string.Join("&", segments)}";
     }
