92625f67f956c4f6191bd5a848082ca643118654
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-25 12:35:18 -0300

    utmTests

---
+++ UtmBuilder.Core.Tests/UtmTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core.Tests;
+
+[TestClass]
+public class UtmTests
+{
+    private Url link = new Url("https://example.io");
+    private Campaign campaign = new Campaign("source", "medium", "name");
+
+    [TestMethod]
+    public void Should_return_URL_from_utm()
+    {
+        var utm = new Utm(link, campaign);
+        utm.ToString();
+        Assert.IsTrue(true);
+    }
+}
