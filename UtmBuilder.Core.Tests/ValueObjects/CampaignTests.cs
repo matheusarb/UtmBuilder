6528c2512652aa522ee1096519ff101cc1a6c342
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-25 11:58:41 -0300

    add CampaignTests

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,12 @@
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class CampaignTests
+{
+    [TestMethod]
+    [DataRow(" ")]
+    public void CampaignTest()
+    {
+
+    }
+}
--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -7,29 +7,31 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 [TestClass]
 public class UrlTests
 {
-    // [TestMethod]
-    // [TestCategory("URL Tests")]
-    //     public void Should_return_an_exception_when_URL_is_invalid()
-    //     {
-    //         try
-    //         {
-    //             var url = new Url("urlInvalida");
-    //             Assert.Fail();
-    //         }
-    //         catch (InvalidUrlException e)
-    //         {                
-    //             Assert.IsTrue(true);
-    //         }
+    private const string InvalidUrl = "urlInvalida";
+    private const string ValidUrl = "https://example.io";
 
-    //     }
+    [TestMethod]
+    [TestCategory("URL Tests")]
+        public void Should_return_an_exception_when_URL_is_invalid()
+        {
+            try
+            {
+                new Url(InvalidUrl);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
     
-    // [TestMethod]
-    // [TestCategory("URL Tests")]
-    // [ExpectedException(typeof(InvalidUrlException))]
-    //     public void Should_return_an_exception_when_URL_is_invalid2()
-    //     {
-    //         new Url("invalidUrl");
-    //     }
+    [TestMethod]
+    [TestCategory("URL Tests")]
+    [ExpectedException(typeof(InvalidUrlException))]
+        public void Should_return_an_exception_when_URL_is_invalid2()
+        {
+            new Url(InvalidUrl);
+        }
 
     [TestMethod]
     [TestCategory("URL Tests")]
@@ -37,7 +39,7 @@ public class UrlTests
         {
             try
             {
-                new Url("https://matheus.io");
+                new Url(ValidUrl);
                 Assert.IsTrue(true);
             }
             catch (InvalidUrlException)
