2f165f7a95cb82be3cc44f938b87642aa555e90d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-25 12:19:19 -0300

    CampaignTests with DataRow

--- UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -1,12 +1,39 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
 namespace UtmBuilder.Core.Tests.ValueObjects;
 
 [TestClass]
 public class CampaignTests
 {
     [TestMethod]
-    [DataRow(" ")]
-    public void CampaignTest()
+    [DataRow("", "", "", true)]
+    [DataRow("src", "", "", true)]
+    [DataRow("src", "med", "", true)]
+    [DataRow("src", "med", "nm", false)]
+    public void CampaignTest(
+        string source,
+        string medium,
+        string name,
+        bool expectException)
     {
 
+        if (expectException)
+        {
+            try
+            {
+                new Campaign(source, medium, name);
+                Assert.Fail();
+            }
+            catch (InvalidCampaignException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        else
+        {
+            new Campaign("source", "medium", "name");
+            Assert.IsTrue(true);
+        }
     }
 }
